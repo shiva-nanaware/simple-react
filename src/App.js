7adfae5ee19172444979fe77d7fe4b95fe0a52ea
Uday <uday@scatterblue.com>
2022-02-28 18:37:55 +0530

    simple change in code

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Testing react app
       </header>
+      <p>This is react testing library for ci-cd pipeline using aws</p>
     </div>
   );
 }
